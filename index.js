36beab3dd951b1eeda3cca3222d1f3e64e3a6ef2
DmitryKomarovit <125357447+DmitryKomarovit@users.noreply.github.com>
2024-05-15 21:10:21 +0500

    init

--- index.js
+++ index.js
@@ -9,64 +9,38 @@ const postsNode = document.querySelector(".js-posts");
 const validationMessage = document.querySelector(".js-validationMessage");
 
 newPostBtnNode.addEventListener("click", function () {
+  if (!postTextInputNode.value || !postTitleInputNode.value) {
+    return;
+  }
+
   const postFromUser = getPostFromUser();
 
   addPost(postFromUser);
 
-  redernPosts();
+  renderPosts();
 
   postTitleInputNode.value = "";
   postTextInputNode.value = "";
-  newPostBtnNode.setAttribute("disabled", true);
 });
 
 postTitleInputNode.addEventListener("input", validation);
 
 postTextInputNode.addEventListener("input", validation);
 
-function validation() {
-  const titleLen = postTitleInputNode.value.length;
-  const textLen = postTextInputNode.value.length;
-
-  if (titleLen > TITLE_VALIDATION_LIMIT) {
-    validationMessage.innerText = `Длинна заголовка привыщает ${TITLE_VALIDATION_LIMIT} символов`;
-    validationMessage.classList.remove("validationMessage_hidden");
-    return;
-  }
-
-  if (textLen > TEXT_VALIDATION_LIMIT) {
-    validationMessage.innerText = `Длинна текста привыщает ${TEXT_VALIDATION_LIMIT} символов`;
-    validationMessage.classList.remove("validationMessage_hidden");
-    return;
-  }
-
-  if (
-    titleLen === 0 ||
-    textLen === 0 ||
-    titleLen > TITLE_VALIDATION_LIMIT ||
-    textLen > TEXT_VALIDATION_LIMIT
-  ) {
-    newPostBtnNode.setAttribute("disabled", true);
-  } else {
-    newPostBtnNode.removeAttribute("disabled");
-  }
-
-  validationMessage.classList.add("validationMessage_hidden");
-}
-
 function getPostFromUser() {
   const title = postTitleInputNode.value;
   const text = postTextInputNode.value;
+  const currentDate = new Date();
+  const date = currentDate.toLocaleString().slice(0, -3);
 
   return {
+    date: date,
     title: title,
     text: text,
   };
 }
 
-function addPost({ title, text }) {
-  let date = new Date();
-
+function addPost({ date, title, text }) {
   posts.unshift({
     date: date,
     title: title,
@@ -78,20 +52,46 @@ function getPosts() {
   return posts;
 }
 
-function redernPosts() {
+function renderPosts() {
   const posts = getPosts();
 
   let postsHTML = "";
 
   posts.forEach((post) => {
     postsHTML += `
-    <div class= 'post'>
-      <p class='post__data'>${post.date.toLocaleString().slice(0, -3)}</p>
+    <div class='post'>
+      <p class='post__data'>${post.date}</p>
       <p class='post__title'>${post.title}</p>
       <p class='post__text'>${post.text}</p>
     </div>
   `;
   });
+  console.log(posts);
 
   postsNode.innerHTML = postsHTML;
+}
+
+function validation() {
+  const titleLen = postTitleInputNode.value.length;
+  const textLen = postTextInputNode.value.length;
+
+  if (titleLen > TITLE_VALIDATION_LIMIT) {
+    validationMessage.innerText = `Длинна заголовка привыщает ${TITLE_VALIDATION_LIMIT} символов`;
+    validationMessage.classList.remove("validationMessage_hidden");
+    newPostBtnNode.setAttribute("disabled", "");
+    return;
+  } else {
+    newPostBtnNode.removeAttribute("disabled");
+  }
+
+  if (textLen > TEXT_VALIDATION_LIMIT) {
+    validationMessage.innerText = `Длинна текста привыщает ${TEXT_VALIDATION_LIMIT} символов`;
+    validationMessage.classList.remove("validationMessage_hidden");
+    newPostBtnNode.setAttribute("disabled", "");
+    return;
+  } else {
+    newPostBtnNode.removeAttribute("disabled");
+  }
+
+  validationMessage.classList.add("validationMessage_hidden");
 }
